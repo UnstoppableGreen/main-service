2d8d4ce98e68d1345e26a1f249a43ace12bc2921
UnstoppableGreen <aleksandr-_kalinin@mail.ru>
2021-12-30 23:44:32 +0300

    fix status

--- src/main/java/ru/rsatu/pojo/Orders.java
+++ src/main/java/ru/rsatu/pojo/Orders.java
@@ -10,10 +10,7 @@ import io.quarkus.hibernate.orm.panache.PanacheEntity;
 @Entity
 //@Table(name="orders")
 public class Orders extends PanacheEntity {
-   /* @Id
-    @SequenceGenerator(name = "orderSeq", sequenceName = "order_id_seq", allocationSize = 1, initialValue = 1)
-    @GeneratedValue(generator = "orderSeq")
-    private Long orderID;*/
+
     private Long clientID;
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER) // order из файла OrdersDetails
     public List<OrdersDetails> orderDetails;
@@ -22,9 +19,7 @@ public class Orders extends PanacheEntity {
     private Date creationDate;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private Date lastUpdateOn;
-	/*public Long getOrderID() {
-		return orderID;
-	}*/
+
 	public Integer getStatusID() {
 		return statusID;
 	}
